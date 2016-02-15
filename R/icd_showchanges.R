3101ae42531822ec488a8b21ad8a5c77c20b2d5e
Ewan Donnachie <ewan.donnachie@kvb.de>
2016-02-15 15:17:03 +0100

    Refactored icd_showchanges for ICD vector input

--- R/icd_showchanges.R
+++ R/icd_showchanges.R
@@ -1,17 +1,23 @@
 #' Show all changes in ICD history relating to the 
-#' 3-digit codes contained in icd_in
+#' 3-digit codes contained in a given vector icd
+#'
+#' @param icd Vector of three-digit ICD codes
+#' @return data.frame with columns YEAR, ICD_CODE, ICD_LABEL and, if specified, DIAG_GROUP
+#' @export
+icd_showchanges_icd3 <- function(icd3_in){
+	subset(icd_hist, icd3 %in% icd3_in & change == TRUE)
+}
+
+#' Show all changes in ICD history relating to the 
+#' 3-digit codes contained in the data.frame icd_in.
+#' The output of icd_expand can be passed directly to this
+#' function to display relevant changes.
+#'
 #' @param icd_in Data frame defining ICD codes of interest
 #' @param col_icd Column of icd_in containing ICD codes (Default: ICD)
 #' @return data.frame with columns YEAR, ICD_CODE, ICD_LABEL and, if specified, DIAG_GROUP
 #' @export
-icd_showchanges <- function(icd_in, year = NULL,
-											 col_icd = "ICD",
-											 label = TRUE){
-	
+icd_showchanges <- function(icd_in, col_icd = "ICD"){
 	icd3_in <- unique(substr(icd_in[, col_icd], 1, 3))
-
-	icd_changes <- subset(icd_hist, icd3 %in% icd3_in & change == TRUE)
-	
-	return(icd_changes)
+	icd_showchanges_icd3(icd3_in)
 }
-

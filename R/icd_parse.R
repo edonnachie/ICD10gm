54ec620c5ef3ece1d9f9d7090ac07dd9f925d4eb
Ewan Donnachie <ewan@donnachie.net>
2018-08-22 20:45:12 +0200

    Improved ICD parsing, applied al to icd_expand with better error handling (closed #3)

--- R/icd_parse.R
+++ R/icd_parse.R
@@ -1,13 +1,21 @@
-regex_icd <- "([A-Z][0-9]{2})\\.?([0-9]{0,2})"
-
+regex_icd <- "([A-Z][0-9]{2})\\.?([0-9\\-]{0,2}) ?[*!\U2020]? ?([GVZA]{0,1})"
+regex_icd_bounded <- paste0("\\b", regex_icd, "[^\\w]?")
+regex_icd_only <- paste0("^", regex_icd, "$")
 
 #' Extract all ICD codes from a character vector
 #'
-#' An ICD code consists of a three digit code
+#' An ICD code consists of, at a minimum,
+#' a three digit ICD-10 code
 #' (i.e. one upper-case letter followed by
-#' two digits), optionally with a two digit
-#' subcode (which may or may not have a period
-#' separating the three-digit code and subcode)
+#' two digits). This may optionally be followed
+#' by a two digit subcode, selected punctuation symbols
+#' (cross "*", dagger "\U2020" or
+#' exclamation mark "!"). Both the period
+#' separating the three-digit code from the subcode,
+#' and the hypthen indicating an "incomplete" subcode,
+#' are optional. Finally, in the ambulatory system, an
+#' additional letter G, V, Z or A may be appended to
+#' signify the status of the diagnosis.
 #'
 #' By default, the function returns a data.frame
 #' containing the matched codes and the standardised
@@ -21,26 +29,42 @@ regex_icd <- "([A-Z][0-9]{2})\\.?([0-9]{0,2})"
 #' separately.
 #'
 #' @param str Character vector from which to extract all ICD codes
-#' @param bind_rows Whether to convert the matrix output of `stirngi::stri_match_all` to a data.frame, with additional `icd_norm` and `icd_sub`
-#'
+#' @param type A character string determining how strictly matching should be performed. This must be one of "strict" (`str` contains a ICD code with no extraneous characters), `bounded` (`str` contains an ICD code with a word boundary on both sides) or `weak` (ICD codes are extracted even if they are contained within a word, e.g. "E10Diabetes" would return "E10"). Default: `bounded`.
+#' @param bind_rows logical. Whether to convert the matrix output of `stirngi::stri_match_all` to a data.frame, with additional `icd_norm` and `icd_sub`
 #' @return data.frame (if bind_rows = TRUE) or matrix
 #' @importFrom stringi stri_match_all_regex
 #' @importFrom dplyr bind_rows
 #' @export
-icd_parse <- function (str, bind_rows = TRUE) {
-  out <- stringi::stri_match_all_regex(str, pattern = regex_icd)
+icd_parse <- function (str, type = "bounded", bind_rows = TRUE) {
+  stopifnot(
+    is.character(str),
+    type %in% c("bounded", "strict", "weak")
+    )
+
+  # Determine regex based on type argument
+  regex_icd_match <- regex_icd_bounded
+  if (type == "weak") regex_icd_match <- regex_icd
+  if (type == "strict") regex_icd_match <- regex_icd_only
+
+  # Parse string and extract components
+  out <- stringi::stri_match_all_regex(str, pattern = regex_icd_match)
 
   if (bind_rows) {
     out <- lapply(out, as.data.frame, stringsAsFactors = FALSE)
     out <- dplyr::bind_rows(out)
-    names(out) <- c("icd_spec", "icd3", "icd_subcode")
+    names(out) <- c("icd_spec", "icd3", "icd_subcode",
+                    "icd_status")
+    out[, "icd_spec"] <- str
     out$icd3[out$icd3 == ""] <- NA
     out$icd_subcode[out$icd_subcode == ""] <- NA
     out$icd_norm = ifelse(!is.na(out$icd3),
                           gsub("\\.?NA", "", paste(out$icd3, out$icd_subcode, sep = ".")),
                           NA_character_)
     out$icd_sub = ifelse(!is.na(out$icd3),
-                         gsub("\\.?NA", "", paste(out$icd3, out$icd_subcode, sep = "")),
+                         gsub("\\.?NA", "",
+                              paste(out$icd3,
+                                    sub("-", "", out$icd_subcode),
+                                    sep = "")),
                          NA_character_)
   }
 
@@ -48,18 +72,41 @@ icd_parse <- function (str, bind_rows = TRUE) {
   return(out)
 }
 
-#' Test whether a string is an ICD code
+#' Test whether a string is a valid ICD code
 #'
-#' An ICD code consists of a three digit code
+#' An ICD code consists of, at a minimum,
+#' a three digit ICD-10 code
 #' (i.e. one upper-case letter followed by
-#' two digits), optionally with a two digit
-#' subcode (which may or may not have a period
-#' separating the three-digit code and subcode)
+#' two digits). This may optionally be followed
+#' by a two digit subcode, selected punctuation symbols
+#' (cross "*", dagger "\\U2020" or
+#' exclamation mark "!"). Both the period
+#' separating the three-digit code from the subcode,
+#' and the hypthen indicating an "incomplete" subcode,
+#' are optional. Finally, in the ambulatory system, an
+#' additional letter G, V, Z or A may be appended to
+#' signify the status of the diagnosis.
 #'
 #' @param str Character vector to be tested
+#' @param year Year for which to test whether the specification is a valid code. Default: NULL (test whether `str` matches a code from any year since 2003)
+#' @param parse logical. Whether to first parse the input `str` using `icd_parse` (Default: TRUE). If FALSE, assumes that `str` is already formatted as `icd_sub` (i.e. without separating period or other punctuation)
 #' @return Logical vector the same length as the character input
 #' @export
-is_icd_code <- function(str) {
-  grepl(pattern = paste0("^", regex_icd, "$"), str)
+is_icd_code <- function(str, year = NULL, parse = TRUE) {
+  # First test whether str matches the pattern of a ICD code,
+  # without any extraneous characters
+  matches_pattern <- grepl(regex_icd_only, str)
+
+  if (parse)
+    str <- ICD10gm::icd_parse(str, type = "strict")$icd_sub
+
+  if (is.null(year) || !(year > 2003 & year < 2100)) {
+    valid_codes <- unique(ICD10gm::icd_meta_codes$icd_sub)
+  } else {
+    valid_codes <- ICD10gm::get_icd_labels(year = year)$icd_sub
+  }
+
+  # Test whether parsed codes are valid for the given year
+  matches_pattern & (str %in% valid_codes)
 }
 
---
+++ tests/testthat/test-icd_expand.R
@@ -0,0 +1,50 @@
+context("test-icd_expand.R")
+
+
+
+test_that("ICD code can be specified as E103, E10.3 or E10.3-", {
+
+  # The following should be identical except for the icd_spec
+  expect_equal(
+    ICD10gm::icd_expand(data.frame(ICD = "E103"), year = 2018)[, -1],
+    ICD10gm::icd_expand(data.frame(ICD = "E10.3"), year = 2018)[, -1]
+    )
+
+  expect_equal(
+    ICD10gm::icd_expand(data.frame(ICD = "E103"), year = 2018)[, -1],
+    ICD10gm::icd_expand(data.frame(ICD = "E10.3-"), year = 2018)[, -1]
+  )
+})
+
+test_that("Throws error if a non-existing ICD code is provided", {
+
+  # The following should be identical except for the icd_spec
+  expect_error(
+    ICD10gm::icd_expand(data.frame(ICD = "R105"), year = 2018)
+  )
+
+})
+
+
+test_that("Expands down the hierarchy", {
+
+  # The following should be identical except for the icd_spec
+  expect_equal(
+    ICD10gm::icd_expand(data.frame(ICD = "A09"), year = 2018)$icd_sub,
+    c("A09", "A090", "A099")
+  )
+
+  expect_equal(
+    ICD10gm::icd_expand(data.frame(ICD = "J44"), year = 2018)$icd_sub,
+    c("J44", "J440", "J4400", "J4401", "J4402", "J4403", "J4409",
+      "J441", "J4410", "J4411", "J4412", "J4413", "J4419",
+      "J448", "J4480", "J4481", "J4482", "J4483", "J4489",
+      "J449", "J4490", "J4491", "J4492", "J4493", "J4499")
+  )
+
+  expect_equal(
+    nrow(ICD10gm::icd_expand(data.frame(ICD = "A0"), year = 2018)),
+    74L
+  )
+
+})

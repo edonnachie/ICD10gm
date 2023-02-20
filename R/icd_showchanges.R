05a20ca18009620156d344e07d80a74a8f331646
Ewan Donnachie <ewan@donnachie.net>
2023-02-20 14:09:20 +0100

    Remove .data$ in tidyselect call

--- R/icd_showchanges.R
+++ R/icd_showchanges.R
@@ -42,21 +42,21 @@ icd_showchanges <- function(icd_in, years=NULL) {
 
   ICD10gm::icd_meta_transition %>%
     # Only interested in codes that have changed
-    dplyr::filter(.data$change) %>%
+    dplyr::filter(change) %>%
     # Restrict to the years of interest
-    dplyr::filter(.data$year_from %in% years) %>%
+    dplyr::filter(year_from %in% years) %>%
     # Restrict to the codes contained in dat_icd
     dplyr::semi_join(icd_in, by = c("icd_from" = "icd_normcode")) %>%
     # Add labels
     dplyr::inner_join(ICD10gm::icd_meta_codes %>%
-                        dplyr::select(.data$icd_normcode, .data$year, .data$label) %>%
-                        dplyr::rename(icd_from_label = .data$label),
+                        dplyr::select(icd_normcode, year, label) %>%
+                        dplyr::rename(icd_from_label = label),
                       by = c("icd_from" = "icd_normcode",
                              "year_from" = "year")) %>%
     dplyr::inner_join(
       ICD10gm::icd_meta_codes %>%
-        dplyr::select(.data$icd_normcode, .data$year, .data$label) %>%
-        dplyr::rename(icd_to_label = .data$label),
+        dplyr::select(icd_normcode, year, label) %>%
+        dplyr::rename(icd_to_label = label),
       by = c("icd_to" = "icd_normcode",
              "year_to" = "year")
     )
